0997df38e54d3070cc1e0e5676a1b3ac03ea9b2e
Jave Lupango <pangga.jave@gmail.com>
2024-04-06 00:10:57 +0800

    Create table using EF migration

---
+++ PizzaPlaceSalesAPI/Migrations/20240405154408_PizzaDbMigration.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PizzaPlaceSalesAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class PizzaDbMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "orders",
+                columns: table => new
+                {
+                    order_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    date = table.Column<string>(type: "varchar(50)", nullable: false),
+                    time = table.Column<string>(type: "varchar(50)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_orders", x => x.order_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pizza_type",
+                columns: table => new
+                {
+                    pizza_type_id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    category = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ingredients = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pizza_type", x => x.pizza_type_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "pizzas",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    pizza_id = table.Column<string>(type: "varchar(50)", nullable: false),
+                    pizza_type_id1 = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    size = table.Column<string>(type: "char(1)", nullable: false),
+                    price = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_pizzas", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_pizzas_pizza_type_pizza_type_id1",
+                        column: x => x.pizza_type_id1,
+                        principalTable: "pizza_type",
+                        principalColumn: "pizza_type_id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "order_details",
+                columns: table => new
+                {
+                    order_details_id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    pizza_idid = table.Column<int>(type: "int", nullable: false),
+                    quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_order_details", x => x.order_details_id);
+                    table.ForeignKey(
+                        name: "FK_order_details_pizzas_pizza_idid",
+                        column: x => x.pizza_idid,
+                        principalTable: "pizzas",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_order_details_pizza_idid",
+                table: "order_details",
+                column: "pizza_idid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_pizzas_pizza_type_id1",
+                table: "pizzas",
+                column: "pizza_type_id1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "order_details");
+
+            migrationBuilder.DropTable(
+                name: "orders");
+
+            migrationBuilder.DropTable(
+                name: "pizzas");
+
+            migrationBuilder.DropTable(
+                name: "pizza_type");
+        }
+    }
+}
